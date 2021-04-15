e437c7481bc7bafd3026a66ef3e24a5143598309
aparna <aparna_patil@yahoo.com>
2021-04-15 01:05:57 +0000

    Modified javaDemo1.java 1st commit of javaDemo2.java

--- javaDemo1.java
+++ javaDemo1.java
@@ -3,6 +3,7 @@ class javaDemo1
 	public static void main(String args[])
 	{
 		System.out.println("This is 1st Java file.");
+		System.out.println("This is 2nd line in this file");
 	}
 }
 
---
+++ javaDemo2.java
@@ -0,0 +1,8 @@
+class javaDemo2
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is 2nd Java file.");
+	}
+}
+
