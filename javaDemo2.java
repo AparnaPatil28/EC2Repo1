ea6a16fe5f9fc9245e50ee1f243ac577cda0a23e
aparna <aparna_patil@yahoo.com>
2021-04-22 01:05:16 +0000

    Line added in javaDemo2.java from feature2.

--- javaDemo2.java
+++ javaDemo2.java
@@ -3,6 +3,7 @@ class javaDemo2
 	public static void main(String args[])
 	{
 		System.out.println("This is 2nd Java file.");
+                System.out.println("This line is added in feature2.");	
 	}
 }
 
